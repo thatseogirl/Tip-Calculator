022646e610477a5cc9273ab59e41650896ae9f05
Tobi <arotimi.tobiloba@gmail.com>
2022-04-07 09:42:31 +0100

    formatting

--- js/app.js
+++ js/app.js
@@ -1,103 +1,90 @@
-const bill = document.getElementById('dollar')
-const tipButons = document.querySelectorAll('.submit')
-const tipCustom = document.getElementById('custom')
-const people = document.getElementById('number')
-const msg = document.querySelector('.error')
-const amount = document.getElementById('tipamount')
-const other = document.getElementById('totalamount')
-const resetBtn = document.getElementById('button')
+const bill = document.getElementById("dollar");
+const tipButons = document.querySelectorAll(".submit");
+const tipCustom = document.getElementById("custom");
+const people = document.getElementById("number");
+const msg = document.querySelector(".error");
+const amount = document.getElementById("tipamount");
+const other = document.getElementById("totalamount");
+const resetBtn = document.getElementById("button");
 
-
-tipButons.forEach(btn => {
-    btn.addEventListener('click', handleClick);
+tipButons.forEach((btn) => {
+  btn.addEventListener("click", handleClick);
 });
 
 function handleClick(event) {
+  let billValue = bill.value;
 
-   let billValue = bill.value;
+  let tipButton = event.target;
+  let tipButtonValue = tipButton.value;
+  let tipButtonValueInteger = parseInt(tipButtonValue);
 
-    let tipButton = event.target;  
-    let tipButtonValue = tipButton.value;  
-    let tipButtonValueInteger = parseInt(tipButtonValue);
+  let billValueInteger = parseInt(billValue);
 
-    let billValueInteger = parseInt(billValue);
+  if (billValueInteger <= 0) {
+    msg.classList.add("error");
+    msg.innerHTML = "Bill can't be zero";
+    return;
+  }
 
-    if (billValueInteger <= 0) {
-        msg.classList.add('error');
-        msg.innerHTML = "Bill can't be zero";
-        return;
-    }
+  let tipAmount = (billValueInteger * tipButtonValueInteger) / (100).toFixed(2);
 
-    let tipAmount = (billValueInteger * tipButtonValueInteger) / 100 .toFixed(2);
+  let totalAmount = (billValueInteger + tipAmount).toFixed(2);
 
-    let totalAmount = (billValueInteger + tipAmount).toFixed(2);
+  let totalPersons = people.value;
+  let totalPersonInteger = parseInt(totalPersons);
 
+  if (totalPersonInteger <= 0) {
+    msg.classList.add("error");
+    msg.innerHTML = "Number can't be zero";
+    return;
+  }
 
-    let totalPersons = people.value;
-    let totalPersonInteger = parseInt(totalPersons);
+  let totalTip = (tipAmount / totalPersonInteger).toFixed(0);
 
-    if (totalPersonInteger <= 0) {
-        msg.classList.add('error');
-        msg.innerHTML = "Number can't be zero";
-        return; 
-    }
+  let totalBill = (totalAmount / totalPersonInteger).toFixed(0);
 
-    let totalTip = (tipAmount/ totalPersonInteger).toFixed(0);
+  if (totalBill <= 0) {
+    return;
+  }
+  if (totalTip <= 0) {
+    return;
+  }
 
-    let totalBill = (totalAmount / totalPersonInteger).toFixed(0);
-
-    if (totalBill <= 0) {
-        return;
-    }
-    if (totalTip <= 0) {
-        return;
-    }
-
-    tipamount.innerHTML = ("$" + totalTip);
-    totalamount.innerHTML = ("$" + totalBill);
+  tipamount.innerHTML = "$" + totalTip;
+  totalamount.innerHTML = "$" + totalBill;
 }
 
-custom.addEventListener('keyup', performCustom);
+custom.addEventListener("keyup", performCustom);
 
 function performCustom(event) {
-    let billValue = bill.value;
+  let billValue = bill.value;
 
-    let customButtonValue = custom.value;
-    let customButtonValueInteger = parseInt(customButtonValue);
-    let billValueInteger = parseInt(billValue);
+  let customButtonValue = custom.value;
+  let customButtonValueInteger = parseInt(customButtonValue);
+  let billValueInteger = parseInt(billValue);
 
-    let customAmount = (billValueInteger * customButtonValueInteger) / 100 .toFixed(2);
+  let customAmount =
+    (billValueInteger * customButtonValueInteger) / (100).toFixed(2);
 
-    let totalPersons = people.value;
-    let totalPersonInteger = parseInt(totalPersons);
+  let totalPersons = people.value;
+  let totalPersonInteger = parseInt(totalPersons);
 
-    let totalCustom = (customAmount/ totalPersonInteger).toFixed(2);
+  let totalCustom = (customAmount / totalPersonInteger).toFixed(2);
 
-    let totalCustomBill = (totalCustom / totalPersonInteger).toFixed(2);
+  let totalCustomBill = (totalCustom / totalPersonInteger).toFixed(2);
 
-    tipamount.innerHTML = ("$" + totalCustom);
-    totalamount.innerHTML = ("$" + totalCustomBill);
-
+  tipamount.innerHTML = "$" + totalCustom;
+  totalamount.innerHTML = "$" + totalCustomBill;
 }
 
-button.addEventListener('click', handleReset);
+button.addEventListener("click", handleReset);
 
 function handleReset(e) {
-    bill.value = 0
-    people.value = 0
-    custom.value = 0
-    let totalTip = 0.00
-    let totalBill = 0.00
-    tipamount.innerHTML = ("$" + totalTip);
-    totalamount.innerHTML = ("$" + totalBill);  
+  bill.value = 0;
+  people.value = 0;
+  custom.value = 0;
+  let totalTip = 0.0;
+  let totalBill = 0.0;
+  tipamount.innerHTML = "$" + totalTip;
+  totalamount.innerHTML = "$" + totalBill;
 }
-
-
-
-
-
-
-
-
-
-
